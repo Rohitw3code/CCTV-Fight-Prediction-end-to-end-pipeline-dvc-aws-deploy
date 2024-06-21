44303e748194b54ffd073aaa4b49f9e643ef9531
Beluga <rohitcode005@gmail.com>
2024-06-21 14:56:50 +0530

    fixed wrong prediction value not normalized

--- src/fightClassifier/prediction.py
+++ src/fightClassifier/prediction.py
@@ -3,10 +3,26 @@ from fightClassifier.components.data_loader import DataLoader
 import numpy as np
 import tensorflow as tf
 
+
+@tf.function
+def single_preprocess(frames: tf.Tensor):
+    """Preprocess the frames tensors and parse the labels."""
+    frames = tf.image.convert_image_dtype(
+        frames[
+            ..., tf.newaxis
+        ],  # The new axis is to help for further processing with Conv3D layers
+        tf.float32,
+    )
+    # Parse label
+    return frames
+
+
 class ModelPredictor:
     def __init__(self,video_file=True):
         self.label_dict = {0: 'noFight', 1: 'Fight'}
         self.model = tf.keras.models.load_model('model/video.keras')
+
+
 
     def video_preprocessing(self,video_file):
         dp = DataPreprocessing('.')
@@ -15,9 +31,9 @@ class ModelPredictor:
         return trim_video
 
     def predict(self,video_file):
-        output = self.model.predict(tf.expand_dims(self.video_preprocessing(video_file), 
-                                                   axis=0))[0]
-
+        tensor = single_preprocess(self.video_preprocessing(video_file))
+        video_ = tf.squeeze(tensor, axis=-1).numpy()
+        output = self.model.predict(tf.expand_dims(video_,axis=0))[0]
         pred = np.argmax(output, axis=0)
         prediction = {
             'output':self.label_dict[pred]
